74b9a23cc1a932f9ab42e1975c92769a098bbfd7
Brendan Berejikian <bezoric1@gmail.com>
2019-02-20 12:05:15 -0500

    Add project files.

---
+++ Area of triangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_of_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
